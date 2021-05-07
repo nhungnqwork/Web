5983e8e59f036af8518dc1fbdf866da3d6a293ca
Alan Nguyen <alannguyen2705.fpt.edu@gmail.com>
2021-05-07 16:30:41 +0700

    dskjd

---
+++ admincp/config/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+	$mysqli = new mysqli("localhost","root","mysql","web_toy");
+
+	// Check connection
+	if ($mysqli->connect_errno) {
+	  echo "Kết nối MYSQLi lỗi" . $mysqli->connect_error;
+	  exit();
+	}
+
+?>
