cb88db458a308be082c5462122f040ddd45efb10
alannguyen2705 <55863320+alannguyen2705@users.noreply.github.com>
2021-05-12 16:54:09 +0700

    Update config.php

--- admincp/config/config.php
+++ admincp/config/config.php
@@ -1,6 +1,7 @@
 <?php 
 
-	$mysqli = new mysqli("localhost","root","mysql","web_toy");
+	$mysqli = new mysqli("postgres://desoffpbmattvu:4522fe8fc7c51d2419d27c66dfdf33076c2f9e2b8f3a1c452cecb9f8a74c1516@ec2-54-163-254-204.compute-1.amazonaws.com:5432/d1er9npltf36tp
+","root","mysql","web_toy");
 
 	// Check connection
 	if ($mysqli->connect_errno) {
